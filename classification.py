1fb3f67808cffc02a796d0c6a864828ea58abfae
Iwona Janny <iwonajanny@hotmail.com>
2020-11-16 22:19:02 +0100

    divide data into two set

--- classification.py
+++ classification.py
@@ -2,5 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+# import tensorflow as tf
 
-df = pd.read_csv('income_evaluation.csv')
+cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+
+df = pd.read_csv('income_evaluation.csv')  
+
+# Feature Matrix 
+x = df.iloc[:, 1:-1].values 
+# Data labels 
+y = df.iloc[:, -1:].values 
+print(y)

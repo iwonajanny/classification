b210bc4a1502f26e410b1f6cdf65e555ce6a8f8f
Iwona Janny <iwonajanny@hotmail.com>
2020-11-17 22:15:35 +0100

    Tensorflow pipeline

--- classification.py
+++ classification.py
@@ -2,14 +2,29 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+df = pd.read_csv('income_evaluation.csv', header = 1, names = cols)
 
-df = pd.read_csv('income_evaluation.csv')  
+#Change values into negative and positive class
+df['income'].replace({' <=50K':0, ' >50K':1}, inplace=True)
 
 # Feature Matrix 
-x = df.iloc[:, 1:-1].values 
+X = df.iloc[:, :-1].values 
 # Data labels 
 y = df.iloc[:, -1:].values 
-print(y)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X , y , test_size=0.2)
+
+#Input pipeline
+def input_pipeline(features_df, target_df, num_of_epochs=2, shuffle=True, batch_size = 20):
+    def input_function():
+        dataset = tf.data.Dataset.from_tensor_slices((dict(features_df), target_df))
+        if shuffle:
+            dataset = dataset.shuffle(1000)
+        dataset = dataset.batch(batch_size).repeat(num_of_epochs)
+        return dataset
+    return input_function
